ef53da4e4347aa03ac2cc6505775f584ff7644fc
unknown <daimonas@abv.bg>
2021-04-28 14:30:28 +0300

    Code formatting.

--- QTRAExample/QTRAExample.ino
+++ QTRAExample/QTRAExample.ino
@@ -30,19 +30,18 @@ QTRSensors qtr;
 
 const uint8_t SensorCount = 6;
 uint16_t sensorValues[SensorCount];
+
 //motors
 int E1 = 10;
 int M1 = 12;
-int E2 =11;
+int E2 = 11;
 int M2 = 13;
 
+void setup() {
 
-void setup()
-{
   // configure the sensors
   qtr.setTypeAnalog();
-  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4}, SensorCount);
-  
+  qtr.setSensorPins((const uint8_t[]) {A0, A1, A2, A3, A4}, SensorCount);
   //qtr.setEmitterPin(2);
 
   delay(500);
@@ -53,24 +52,21 @@ void setup()
   // 0.1 ms per sensor * 4 samples per sensor read (default) * 6 sensors
   // * 10 reads per calibrate() call = ~24 ms per calibrate() call.
   // Call calibrate() 400 times to make calibration take about 10 seconds.
-  for (uint16_t i = 0; i < 400; i++)
-  {
+  for (uint16_t i = 0; i < 400; i++) {
     qtr.calibrate();
   }
   digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
 
   // print the calibration minimum values measured when emitters were on
   Serial.begin(9600);
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
+  for (uint8_t i = 0; i < SensorCount; i++) {
     Serial.print(qtr.calibrationOn.minimum[i]);
     Serial.print(' ');
   }
   Serial.println();
 
   // print the calibration maximum values measured when emitters were on
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
+  for (uint8_t i = 0; i < SensorCount; i++) {
     Serial.print(qtr.calibrationOn.maximum[i]);
     Serial.print(' ');
   }
@@ -82,8 +78,8 @@ void setup()
   pinMode(M2, OUTPUT);
 }
 
-void loop()
-{
+void loop(){
+  
   Motor()
   // read calibrated sensor values and obtain a measure of the line position
   // from 0 to 5000 (for a white line, use readLineWhite() instead)
@@ -92,33 +88,24 @@ void loop()
   // print the sensor values as numbers from 0 to 1000, where 0 means maximum
   // reflectance and 1000 means minimum reflectance, followed by the line
   // position
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
+  for (uint8_t i = 0; i < SensorCount; i++) {
     Serial.print(sensorValues[i]);
     Serial.print('\t');
   }
-  
   Serial.println(position);
-
   delay(250);
-
-
-{int value;
-for(value = 0 ; value <= 255; value+=5)
-{
-digitalWrite(M1,HIGH);
-digitalWrite(M2, HIGH);
-analogWrite(E1, value);
-analogWrite(E2, value);
-digitalWrite(M1,HIGH);
-digitalWrite(M2, HIGH);
-analogWrite(E1, value);
-analogWrite(E2, value);
-delay(30);
-}
-delay(1000);
-}
-
-
-
+  
+  int value;
+  for (value = 0 ; value <= 255; value += 5) {
+    digitalWrite(M1, HIGH);
+    digitalWrite(M2, HIGH);
+    analogWrite(E1, value);
+    analogWrite(E2, value);
+    digitalWrite(M1, HIGH);
+    digitalWrite(M2, HIGH);
+    analogWrite(E1, value);
+    analogWrite(E2, value);
+    delay(30);
+  }
+  delay(1000);
 }
